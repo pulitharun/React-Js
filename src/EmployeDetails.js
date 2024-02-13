ce7147c32f35e953624cb8130ec8c0d92239a02e
Puli <66771051+pulitharun@users.noreply.github.com>
2024-02-13 15:39:37 +0530

    Update EmployeDetails.js

--- src/EmployeDetails.js
+++ src/EmployeDetails.js
@@ -5,16 +5,12 @@ import EMPLOYEDATE from './employeedate';
 function EMPLOYEDETAILS(props){
 
     return(
-        <div>
-        <div className='Employee-details'>
-        <div className='Employee-details-dob'>
-        <EMPLOYEDATE date={props.dob}/>
-        </div>
-        <div><h2>{props.name}</h2></div>
-        <div className='Employee-details-exp'>{props.exp}</div>
-        
+     <div>
+            <div className='Employee-details'></div>
+                 <div><h2>{props.name}</h2></div>
+                 <EMPLOYEDATE date={props.dob}/>
+                 <div>{props.exp}</div>
     </div>
-        </div>
     );
 }
 export default EMPLOYEDETAILS
